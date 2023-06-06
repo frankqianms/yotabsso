e44fd7abe3494a5636d04d20b86a75a8d94a777c
frankqianms <frankqian@microsoft.com>
2023-06-06 10:27:21 +0800

    init

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,8 @@ import * as morgan from "morgan";
 import { MsTeamsApiRouter, MsTeamsPageRouter } from "express-msteams-host";
 import * as debug from "debug";
 import * as compression from "compression";
+import * as fs from "fs";
+import * as chalk from "chalk";
 
 // Initialize debug logging module
 const log = debug("msteams");
@@ -20,9 +22,26 @@ import * as allComponents from "./TeamsAppsComponents";
 
 // Create the Express webserver
 const express = Express();
-const port = process.env.port || process.env.PORT || 3007;
+// const port = process.env.port || process.env.PORT || 3007;
+const port = 53000;
+
+function readEnvFile(file, type) {
+    if (!fs.existsSync(file)) {
+        throw new Error(
+            `You specified ${chalk.cyan(
+                type
+            )} in your env, but the file "${chalk.yellow(file)}" can't be found.`
+        );
+    }
+    return fs.readFileSync(file);
+}
+// eslint-disable-next-line camelcase
+const local_cert = readEnvFile(process.env.SSL_CRT_FILE, "SSL_CRT_FILE");
+// eslint-disable-next-line camelcase
+const local_key = readEnvFile(process.env.SSL_KEY_FILE, "SSL_KEY_FILE");
 
 // Inject the raw request body onto the request object
+
 express.use(Express.json({
     verify: (req, res, buf: Buffer, encoding: string): void => {
         (req as any).rawBody = buf.toString();
@@ -61,6 +80,9 @@ express.use("/", Express.static(path.join(__dirname, "web/"), {
 
 // Set the port
 express.set("port", port);
+express.set("local_cert", local_cert);
+console.log("local_cert", express.get("local_cert"));
+express.set("local_key", local_key);
 
 // Start the webserver
 http.createServer(express).listen(port, () => {
